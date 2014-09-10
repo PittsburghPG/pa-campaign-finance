5cc7e76175960e60bad277e7039cbae383b2da81
Andrew McGill <arm5077@gmail.com>
2014-09-10 16:58:44 -0400

    added filer/filings tables and reworked original

--- sql/init.sql
+++ sql/init.sql
@@ -6,14 +6,65 @@ CREATE DATABASE campaign_finance;
 DROP TABLE IF EXISTS campaign_finance.contributions;
 CREATE TABLE campaign_finance.contributions
 (
-	id 				serial NOT NULL,
-	filerid 		char varying(8),
-	year 			int,
-	cycle 			char(1),
-	section 		char(2),
-	contributor 	char(100),
-	address 		char(150),
-	address2 		char(50),
-	
-	
-)
+	`id` 				bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	`filerid` 			char varying(8) 	DEFAULT NULL,
+	`year` 				int(11) 			DEFAULT NULL,
+	`cycle` 			char varying(1) 	DEFAULT NULL,
+	`section` 			char varying(2) 	DEFAULT NULL,
+	`contributor` 		char varying(100) 	DEFAULT NULL,
+	`address` 			char varying(150) 	DEFAULT NULL,
+	`address2` 			char varying(50) 	DEFAULT NULL,
+	`city` 				char varying(50) 	DEFAULT NULL,
+	`state` 			char varying(3) 	DEFAULT NULL,
+	`zip` 				char varying(10) 	DEFAULT NULL,
+	`newzip` 			char varying(10) 	DEFAULT NULL,
+	`occupation` 		char varying(100) 	DEFAULT NULL,
+	`empName` 			char varying(100) 	DEFAULT NULL,
+	`empAddress1` 		char varying(150) 	DEFAULT NULL,
+	`empAddress2` 		char varying(50) 	DEFAULT NULL,
+	`empCity` 			char varying(50) 	DEFAULT NULL,
+	`empState` 			char varying(3) 	DEFAULT NULL,
+	`unformattedDate` 	char varying(8) 	DEFAULT NULL,
+	`date` 				date 				DEFAULT NULL,
+	`contribution` 		float 				DEFAULT NULL,
+	`description` 		char varying(200) 	DEFAULT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `id` (`id`)
+);
+
+/* Create filer table */
+DROP TABLE IF EXISTS campaign_finance.filers;
+CREATE TABLE campaign_finance.filers
+(
+	`filerid` 			char varying(8)		DEFAULT NULL,
+	`type`				int					DEFAULT NULL,
+	`name`				char varying(100)	DEFAULT NULL,
+	`office`			char varying(100)	DEFAULT NULL,
+	`district`			char varying(50)	DEFAULT NULL,
+	`party`				char varying(50)	DEFAULT NULL,
+	`address1`			char varying(150)	DEFAULT NULL,
+	`address2`			char varying(50)	DEFAULT NULL,
+	`city`				char varying(50)	DEFAULT NULL,
+	`state`				char varying(3)		DEFAULT NULL,
+	`zip`				char varying(10)	DEFAULT NULL,
+	`county`			char varying(2)		DEFAULT NULL,
+	`phone`				char varying(12)	DEFAULT NULL,
+	PRIMARY KEY (`filerid`),
+	UNIQUE KEY `filerid` (`filerid`)
+);
+
+/* Create filings table */
+DROP TABLE IF EXISTS campaign_finance.filings;
+CREATE TABLE campaign_finance.filings
+(
+	`id` 					bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	`filerid` 			char varying(8)		DEFAULT NULL,
+	`year`				int					DEFAULT NULL,
+	`ammend_flag`		char(1)				DEFAULT NULL,
+	`terminated_flag`	char(1)				DEFAULT NULL,
+	`beginning`			float				DEFAULT NULL,
+	`monetary`			float				DEFAULT NULL,
+	`inkind`			float				DEFAULT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `id` (`id`)
+);
