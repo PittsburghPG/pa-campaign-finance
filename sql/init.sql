cb9bcd7f875f3831c37dff2fa5ffc35855efaa08
Andrew McGill <arm5077@gmail.com>
2014-09-10 11:14:46 -0400

    Add database/main table initialization

--- sql/init.sql
+++ sql/init.sql
@@ -1,1 +1,19 @@
- 
+ /* Create contribution schema */
+DROP DATABASE IF EXISTS campaign_finance;
+CREATE DATABASE campaign_finance;
+
+/* Create main contributions table */
+DROP TABLE IF EXISTS campaign_finance.contributions;
+CREATE TABLE campaign_finance.contributions
+(
+	id 				serial NOT NULL,
+	filerid 		char varying(8),
+	year 			int,
+	cycle 			char(1),
+	section 		char(2),
+	contributor 	char(100),
+	address 		char(150),
+	address2 		char(50),
+	
+	
+)
