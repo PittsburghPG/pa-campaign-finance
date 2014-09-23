a79dbd1bb69b2373bc5b6bf2c4eb5c32812c6f63
Alexandra Kanik <alexandra@rationalact.com>
2014-09-23 18:09:47 -0400

    merge laura branch with frontend branch
    make css and html consistent

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
- 
+var map;
+$(document).ready(function() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(40.4450813, -80.00877459999998),
+	disableDefaultUI: true
+  };
+  map = new google.maps.Map(document.getElementById('map_canvas'),
+      mapOptions);
+
+});
