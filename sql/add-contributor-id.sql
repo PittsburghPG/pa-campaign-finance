6c3da887518eadd8bbc4e79432142f47f2ee73b6
Andrew McGill <arm5077@gmail.com>
2014-09-25 10:18:15 -0400

    add contributorid generation

---
+++ sql/add-contributor-id.sql
@@ -0,0 +1,49 @@
+DELIMITER ;;
+DROP PROCEDURE IF EXISTS add_contributorid ;;
+CREATE PROCEDURE add_contributorid()
+BEGIN
+	IF NOT EXISTS (
+		SELECT * 
+		FROM 	information_schema.columns 
+		WHERE 	table_schema=DATABASE() 
+				AND table_name='contributions' 
+				AND column_name='contributorid') 
+	THEN
+		ALTER TABLE campaign_finance.contributions
+		ADD 	contributorid bigint(20)	 unsigned 	not null;
+	end if;
+	
+	DROP TABLE IF EXISTS campaign_finance.contributors;
+	CREATE TABLE campaign_finance.contributors
+		(SELECT 	contributor, 
+					address, 
+					city, 
+					state, 
+					zip 
+	
+		FROM 		campaign_finance.contributions 
+		GROUP BY contributor, 
+					address, 
+					city, 
+					state, 
+					zip);
+
+	ALTER TABLE campaign_finance.contributors
+	ADD 	`contributorid`		bigint(20) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY;
+
+	UPDATE 		campaign_finance.contributions
+	JOIN 		campaign_finance.contributors 
+	ON 			contributions.contributor = contributors.contributor
+	AND 		contributions.address = contributors.address
+	AND 		contributions.city = contributors.city
+	AND 		contributions.state = contributors.state
+	AND 		contributions.zip = contributors.zip
+	SET 		contributions.contributorid = contributors.contributorid;
+	
+END ;;
+
+CALL add_contributorid() ;;
+
+DROP PROCEDURE IF EXISTS add_contributorid ;;
+
+DELIMITER ;
