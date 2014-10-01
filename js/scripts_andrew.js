b8d601598529cbc8310a669faa324d5e79bd8d81
Andrew McGill <arm5077@gmail.com>
2014-09-30 23:08:31 -0400

    added timechart

--- js/scripts_andrew.js
+++ js/scripts_andrew.js
@@ -40,7 +40,6 @@ $(document).ready(function(){
 				else if ( i == 0 ) data.push({ label: "Out of state", data: parseFloat(state.amount)}) 
 				else data[0].data += parseFloat(state.amount);
 			});
-			console.log(data);
 			$.plot('#pie', data, {
 				series: {
 					pie: {
@@ -50,6 +49,89 @@ $(document).ready(function(){
 			});
 		});
 	
+	// END in-state v. out-of-state
 	
+	// Start time series 
+	function makeTimeChart(id, filerid, startDate, endDate){
+		$.getJSON("api/months/candidates/" + filerid + "?startDate=" + startDate + "&endDate=" + endDate, function(json){
+			
+			data = [];
+			$.each(json.results, function(i, date){
+				data.push( [Date.parse(date["year"] + "-" + date["month"] + "-" + "01"), date["total"]] );
+				
+				
+				$.plot("#" + id, [{ 
+					data: data, 
+					color:"seagreen"
+					
+					
+				}], {
+					series: {
+						lines:{
+							lineWidth: 6
+						}
+					},
+					xaxis: { 
+						mode: "time",
+						min: data[0][0]	
+					},
+					yaxis: {
+						tickFormatter: function(val, axis){
+							return "$" + numberWithCommas(val);
+						}
+					},
+					grid: {
+						borderWidth: {
+							top: 0,
+							left: 1,
+							right: 0,
+							bottom: 1
+						},
+						hoverable: true
+					},
+					markings:0
+				});
+				
+				$("#" + id).bind("plothover", function(event, pos, item){
+					
+					if( item ) {
+						if( $("#tooltip").length == 0 ){
+							$("<div id='tooltip'></div>").appendTo( $("body") )
+								.css({top: item.pageY+5, left: item.pageX+5});
+							$("#tooltip").html(toDollar(item.datapoint[1]));
+						}
+						else {
+							console.log(toDollar(item.datapoint[1]));
+							$("#tooltip").css({top: item.pageY-20, left: item.pageX+10});
+						}
+						x = item.datapoint[0].toFixed(2);
+						y = item.datapoint[1].toFixed(2);
+						
+					}
+						else $("#tooltip").remove();
+					
+					
+				});
+				
+				
+				
+			});
+			
+		
+		});
+	}
+		
+	// end time series
+	
+	
+	makeTimeChart("timechart", 20130153, "2014-01-01", "2014-09-01");
 	
 });
+
+function toDollar(x){
+	return "$" + numberWithCommas( Math.floor(x * 100) / 100 );
+}
+
+function numberWithCommas(x){
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
