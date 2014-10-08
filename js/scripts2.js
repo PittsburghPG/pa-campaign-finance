eb23c20165bef5400a236e67a32fcb0beeb08ddf
Alexandra Kanik <alexandra@rationalact.com>
2014-10-08 18:50:25 -0400

    fixing county map issue

--- js/scripts2.js
+++ js/scripts2.js
@@ -308,9 +308,10 @@ $(document).ready(function() {
 				var mapSection;
 				if (countyState != "PA"){
 					widthTotals = "col-lg-12 col-md-12 col-sm-12";
-					mapSection = $("<div class='col-lg-5 col-md-5 col-sm-4 col-xs-12'> \
+					/*mapSection = $("<div class='col-lg-5 col-md-5 col-sm-4 col-xs-12'> \
 														<svg id='map' style='width:100%; height:425px;'></svg> \)
-													</div>").appendTo(mapAndTotals);
+													</div>");
+					mapSection.appendTo(mapAndTotals);*/
 				}else{
 					widthTotals = "col-lg-7 col-md-7 col-sm-8"
 				};
